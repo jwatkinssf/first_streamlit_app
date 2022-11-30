f5e1ab6c427ee93294887d69f7a2ed0be168cb13
jwatkinssf <118755766+jwatkinssf@users.noreply.github.com>
2022-11-30 15:21:38 -0700

    Update streamlit_app.py
    Function and query button add

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,22 @@ except URLError as e:
     streamlit.error()
 
 #Lesson 12
-streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# SlowFlake Related Functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
 
+# Add a button to load the fruit
+if steamlit.button('Get Freuit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+
+streamlit.stop()    
 # add second text box
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Banana')
 streamlit.write('Thanks for adding ', add_my_fruit)
