e7ef9dc70399ae091b728c469237444d322e80ec
jwatkinssf <118755766+jwatkinssf@users.noreply.github.com>
2022-11-30 15:04:02 -0700

    Update streamlit_app.py
    add try-except - If then else

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+        streamlit.error("Please select a fuirt to get information")
+   else:
+   #streamlit.write('The user entered ', fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   #streamlit.text(fruityvice_response.json()) #writes data to screen
+   # normalize the json response 
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # output the response as a table
+   streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #writes data to screen
-
-# normalize the json response 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the response as a table
-streamlit.dataframe(fruityvice_normalized)
+  except URLError as e:
+      streamlit.error()
 
 #Lesson 12
 streamlit.stop()
